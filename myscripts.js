8afc9c527f976bea2a66e4de1711c446026de89d
muratpala007 <muratpala007@gmail.com>
2022-04-19 22:40:48 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+function ozellikleriDegistir(){
+        var paragrafimiz = document.getElementsByClassName("p1")[0];
+        paragrafimiz.style.color = "orange";
+        paragrafimiz.style.border = "10px solid pink"
+        paragrafimiz.style.width = "75%";
+        paragrafimiz.style.backgroundColor = "turquoise";
+        paragrafimiz.style.textAlign = "left";
+    }
